9917776620738ba1032bfe16aebbdb227bff1907
jinteng-li <102038542+jinteng-li@users.noreply.github.com>
2022-04-07 14:05:38 +0100

    Update split_data.py

--- split_data.py
+++ split_data.py
@@ -4,15 +4,21 @@ import shutil
 cur_dir = os.getcwd()
 train_data_path = os.path.join(cur_dir, "data", "train")
 val_data_path = os.path.join(cur_dir, "data", "val")
+
+# num of images that will move to validation set(per class)
+# decide train-val split ratio, since originally trainning set have 50 images per class
+# ratio = (50-10) : 10 = 4:1
 num_val = 10
 
 train_list = os.listdir(train_data_path)
 
 os.chdir(val_data_path)
+# creating 1000 class folder in the validation directory
 for item in train_list:
     if item not in os.listdir(val_data_path):
         os.mkdir(item)
 
+# moving samples from training set folder to validation set folder
 for dir in train_list:
     source_train_dir = os.path.join(train_data_path, dir)
     target_val_dir = os.path.join(val_data_path, dir)
